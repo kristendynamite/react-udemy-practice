519ec5562c885d303ac21b3c3b2d0ca0e5b2be08
Kristen <kristen.dynamite@gmail.com>
2017-09-14 14:34:10 -0700

    add searach functionality to search bar

--- src/index.js
+++ src/index.js
@@ -16,18 +16,23 @@ class App extends Component {
       selectedVideo: null
     };
 
-    YTSearch({key: API_KEY, term: 'gwen stefani'}, (videos) => {
+    this.videoSearch('gwen stefani');
+  }
+
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
       this.setState({
         videos:videos,
         selectedVideo: videos[0]
-       });
+      });
     });
   }
 
   render(){
     return  (
       <div>
-      <SearchBar />
+      <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
       <VideoDetail video={this.state.selectedVideo}/>
       <VideoList
         onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
